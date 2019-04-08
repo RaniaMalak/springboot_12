1a4cd001d5b07c6406758464476ac0a515ec2a5d
GBTC440005UR <GBTC440005UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-04-08 19:51:16 -0400

    Initial commit

---
+++ src/main/java/com/example/springboot_12/Actor.java
@@ -0,0 +1,49 @@
+package com.example.springboot_12;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Actor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getRealname() {
+        return realname;
+    }
+
+    public String getHeadshot() {
+        return headshot;
+    }
+
+    private String name;
+    private String realname;
+    private String headshot;
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setRealname(String realname) {
+        this.realname = realname;
+    }
+
+    public void setHeadshot(String headshot) {
+        this.headshot = headshot;
+    }
+}
---
+++ src/main/java/com/example/springboot_12/ActorRepository.java
@@ -0,0 +1,8 @@
+package com.example.springboot_12;
+
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ActorRepository extends CrudRepository<Actor,Long>{
+
+}
